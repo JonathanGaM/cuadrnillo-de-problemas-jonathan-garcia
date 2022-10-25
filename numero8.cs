4b2c684a56a1352490a839a76d202b6b1bcc4b42
JonathanGaM <116600747+JonathanGaM@users.noreply.github.com>
2022-10-25 17:51:18 -0500

    Add files via upload

---
+++ numero8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esperanzaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diastrabajado, suelsemana, tsueldosemanal;
+
+            Console.WriteLine("Ingresa el sueldo semanal");
+            suelsemana = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa los dias trabajados");
+            diastrabajado = Double.Parse(Console.ReadLine());
+            tsueldosemanal = suelsemana / 6 * diastrabajado;
+            Console.WriteLine("El total del sueldo semanal es: " + tsueldosemanal);
+        }
+    }
+}
+
